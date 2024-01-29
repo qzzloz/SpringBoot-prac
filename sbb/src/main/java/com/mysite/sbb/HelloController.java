0e5a56266b7a863b0ff6412c2868371529f9f479
qzzloz <xlivsxd@gmail.com>
2024-01-29 16:31:30 +0900

    Initial commit

---
+++ sbb/src/main/java/com/mysite/sbb/HelloController.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String Hello(){
+        return "Hello World vbbb";
+    }
+}
