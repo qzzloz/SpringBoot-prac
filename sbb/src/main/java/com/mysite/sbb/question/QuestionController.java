d7996eab3d18c2fc14c9026b34d44326f94abc0d
qzzloz <xlivsxd@gmail.com>
2024-02-15 14:23:23 +0900

    v2.8 서비스 클래스 추가

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
